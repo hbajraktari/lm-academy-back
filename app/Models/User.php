39f64ba0dc118e7d2d3b205461207e94e6b7202c
hbajraktari <hysnije.bb1@gmail.com>
2025-03-01 02:56:27 -0800

    creating scoreboard model/migration and its relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 use App\Models\UserInfo;
 use App\Models\UserList;
+use App\Models\Scoreboard;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -54,10 +55,16 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function scopeSelectSomeUserData($query){
+        return $query->select('users.id','first_name', 'last_name','gender','email','image');
+    }
     public function UserInfo(){
         return $this->hasOne(UserInfo::class, 'user_id', 'id');
     }
     public function lists(){
         return $this->belongsToMany(UserList::class, 'user_list_items','user_id','list_id')->withTimestamps();
     }
+    public function scoreOnScoreboard(){
+        $this->hasOne(Scoreboard::class, 'user_id');
+   }
 }
