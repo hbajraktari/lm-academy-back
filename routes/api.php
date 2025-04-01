6c535e72d7f0b67590f622c06cc35e1bbacb3491
hbajraktari <hysnije.bb1@gmail.com>
2025-04-01 08:48:02 -0700

    Config: gmail mail service, created a test-mail view, mailable & endpoint to test e-mail with laravel

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Mail\TestMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
@@ -9,6 +11,7 @@ Route::controller(AuthController::class)->prefix("auth")->middleware('api')->gro
     Route::post('login', 'login')->name("auth.login");
     Route::post('register', 'register')->name("auth.register");
     Route::post('refresh', 'refresh')->name('auth.refresh');
+
 
     Route::middleware('jwt.auth.token')->group(function () {
         Route::post("logout", "logout")->name("auth.logout");
@@ -23,3 +26,28 @@ Route::controller(AuthController::class)->prefix("auth")->middleware('api')->gro
 // Route::get('test-api-endpoint', function() {
 //     return response()->json(['message' => 'API endpoint is working']);
 // });
+
+Route::post('test-mail-sent', function(Request $request) {
+    try {
+        $mailData = [
+            'title' => 'Email Title',
+            'message' => 'This is a test e-mail directed to only students of Lutfi Musiqi High School.',
+            'session_title' => $request->session_title
+    
+        ];
+
+        $cc_users = [];
+        Mail::to('hysnije.bb1@gmail.com')->send(new TestMail($mailData));
+
+        return response()->json('success');
+    } catch (Exception $e) {
+        return response()->json([
+            'success' => false,
+            'error' => [
+            'code' => $e->getCode(),
+            'message' => $e->getMessage(),
+            'type' => class_basename($e)
+            ]
+        ], 500);
+    }
+});
