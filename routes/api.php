a0efa7a1747d6307e10c4d944f371171fa5f45c4
hbajraktari <hysnije.bb1@gmail.com>
2025-04-11 09:35:27 -0700

    adding the zen quotes api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Mail\TestMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -50,4 +51,38 @@ Route::post('test-mail-sent', function(Request $request) {
             ]
         ], 500);
     }
+});
+
+Route::get('zen-quote', function() {
+
+    try {
+        $response = Http::get("https://zenquotes.io/api/random");
+
+    if($response->successful()) {
+        $quote = $response->json()[0];
+
+        return response()->json([
+            'success' => true,
+            'quote' => [
+                'text' => $quote['q'],
+                'author' => $quote['a'],
+            ]
+        ]);
+    }
+
+
+    return response()->json([
+        'success' => false,
+        'message' => 'Failed to fetch quote from external API'
+    ]);
+
+    } catch (\Exception $e) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Failed to fetch quote from external API',
+            'error' => 'Failed to fetch quote',
+            'details' => $e->getMessage()
+        ]);
+    }
+
 });
